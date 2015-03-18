48c5e1855b4813f2c82eb2025fc711372c1283a0
cullenohara <cullenohara@gmail.com>
2015-03-17 22:50:20 -0500

    Add files?
    showing files still in github

---
+++ Assets/scripts/Quests.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Quests : MonoBehaviour {
+
+	public string name;
+	public string text;
+	public string item;
+	public int amount;
+
+	public Quests(string questName, string questText, string questItem, int itemAmount)
+	{
+		name = questName;
+		text = questText;
+		item = questItem;
+		amount = itemAmount;
+	}
+}
