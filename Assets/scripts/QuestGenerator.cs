5a71c1a824d0847467b9ba3d63ae337baafabaa2
cullenohara <cullenohara@gmail.com>
2015-03-17 22:49:14 -0500

    Quest Gen
    Files added on Emily's terrible laptop

---
+++ Assets/scripts/QuestGenerator.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class QuestGenerator : MonoBehaviour {
+	
+	public List<Quests> QuestList = new List<Quests>();
+
+	void Start () 
+	{
+
+	}
+
+	void Update () 
+	{
+		
+	}
+}
