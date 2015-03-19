39579d9cd5f4d346684724771d0f732c048a2fa8
Cullen O'Hara <cullenohara@gmail.com>
2015-03-18 21:35:57 -0600

    changed file structure
    reorganized file structure.

---
+++ Assets/scripts/Map/MapGenerator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour {
+
+	public int mapSize = 5;
+	public float tileOffset = 1.2f;
+	public Transform tile;
+
+	private Transform tempTile;
+
+	// Use this for initialization
+	void Start () 
+	{
+		for(int i = 0; i < mapSize; i++)
+		{
+			//tempTile = Instantiate(tile, new Vector3(i + tileOffset, 0, 0), transform.rotation);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
